16beaa50a07346d72e5d60964b00e9bcc342d42f
Krisna <krisnaerlangga08@gmail.com>
2022-11-07 23:45:45 +0700

    progress utility 1 :rocket:

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./public/**/*.{html, js}"],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        13: "11rem",
+      },
+      fontFamily: {
+        poppins: ["Poppins"],
+      },
+    },
   },
   plugins: [],
 };
